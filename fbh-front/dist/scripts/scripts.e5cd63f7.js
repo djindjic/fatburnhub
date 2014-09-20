d325c512f850aa00a932376340691ea980760c94
Aleksandar Djindjic <djindjic@gmail.com>
2014-09-20 15:30:54 +0200

    [deploy: fatburnhub]

--- fbh-front/dist/scripts/scripts.e5cd63f7.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("fbhFrontApp",["ngAnimate","ngCookies","ngResource","ngRoute","ngSanitize","ngTouch"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("fbhFrontApp").controller("MainCtrl",["$scope",function(){}]);
