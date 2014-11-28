67bc622439cd2a89d70ef56cb46e5736bba32ce0
Aleksandar Djindjic <djindjic@gmail.com>
2014-11-28 23:31:05 +0100

    [deploy: cloudatcost]

--- fbh-front/www/config.js
+++ fbh-front/www/config.js
@@ -2,32 +2,6 @@ System.config({
   "paths": {
     "*": "*.js",
     "github:*": "jspm_packages/github/*.js"
-  },
-  "bundles": {
-    "build": [
-      "github:systemjs/plugin-css@0.1.0/css",
-      "github:driftyco/ionic-bower@1.0.0-beta.13/js/ionic",
-      "github:angular/bower-angular@1.3.4/angular.min",
-      "github:angular/bower-angular-animate@1.3.4/angular-animate",
-      "github:angular/bower-angular-sanitize@1.3.4/angular-sanitize",
-      "github:angular-ui/ui-router@0.2.10/release/angular-ui-router",
-      "github:firebase/firebase-bower@2.0.5/firebase",
-      "app/routes/diet/diet.template.html!github:systemjs/plugin-text@0.0.2",
-      "app/routes/training/training.template.html!github:systemjs/plugin-text@0.0.2",
-      "github:systemjs/plugin-css@0.1.0",
-      "github:angular/bower-angular@1.3.4",
-      "github:angular/bower-angular-animate@1.3.4",
-      "github:angular/bower-angular-sanitize@1.3.4",
-      "github:angular-ui/ui-router@0.2.10",
-      "github:firebase/firebase-bower@2.0.5",
-      "app/routes/diet/diet.route",
-      "app/routes/training/training.route",
-      "github:firebase/angularfire@0.9.0/angularfire",
-      "github:driftyco/ionic-bower@1.0.0-beta.13/js/ionic-angular",
-      "github:firebase/angularfire@0.9.0",
-      "github:driftyco/ionic-bower@1.0.0-beta.13",
-      "app/main"
-    ]
   }
 });
 
