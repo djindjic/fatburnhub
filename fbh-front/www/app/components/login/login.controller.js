005de2c696a968c43d083059f8255e858c889b3e
Aleksandar Djindjic <djindjic@gmail.com>
2014-12-04 13:19:51 +0100

    directive link

--- fbh-front/www/app/components/login/login.controller.js
+++ fbh-front/www/app/components/login/login.controller.js
@@ -2,38 +2,5 @@ import angular from 'angular';
 
 export let loginControllerModule = angular.module('loginControllerModule', [])
 .controller('LoginController', ['$scope', function($scope) {
-  var ref = new Firebase($scope.firebaseUrl);
-
-  ref.onAuth(function(authData) {
-	  if (authData) {
-	  	ref.child('users').child(authData.uid).set(authData);
-	    // user authenticated with Firebase
-	    console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
-	    $scope.uid = authData.uid;
-	    $scope.authenticated = true;
-	  } else {
-	  	$scope.authenticated = false;
-	  	//  window.cookies.clear(function() {
-		  //   console.log("Cookies cleared!");
-		  // });
-	    console.log("user is logged out");
-	  }
-	});
-
-  // Logs a user in with inputted provider
-  $scope.login = function(provider) {
-  	$scope.authenticated = true;
-    ref.authWithOAuthPopup("facebook", function(error, authData) {
-    	if (authData) {
-		    // the access token will allow us to make Open Graph API calls
-		    console.log(authData.facebook.accessToken);
-		  }
-    }, {
-	  remember: "default",
-	  scope: "email"
-	});
-  };
-  $scope.logout = function(provider) {
-    ref.unauth();
-  };
+  
 }]);
--- fbh-front/www/app/components/login/login.directive.js
+++ fbh-front/www/app/components/login/login.directive.js
@@ -12,6 +12,43 @@ export let loginDirectiveModule = angular.module('loginDirectiveModule', [
 	  firebaseUrl: '@firebaseUrl'
 	},
     controller: 'LoginController',
-    template: template
-  };
+    template: template,
+   link: function(scope) {
+      console.log(scope);
+      var ref = new Firebase(scope.firebaseUrl);
+
+	  ref.onAuth(function(authData) {
+		  if (authData) {
+		  	ref.child('users').child(authData.uid).set(authData);
+		    // user authenticated with Firebase
+		    console.log("User ID: " + authData.uid + ", Provider: " + authData.provider);
+		    scope.uid = authData.uid;
+		    scope.authenticated = true;
+		  } else {
+		  	scope.authenticated = false;
+		  	//  window.cookies.clear(function() {
+			  //   console.log("Cookies cleared!");
+			  // });
+		    console.log("user is logged out");
+		  }
+		});
+
+	  // Logs a user in with inputted provider
+	  scope.login = function(provider) {
+	  	scope.authenticated = true;
+	    ref.authWithOAuthPopup("facebook", function(error, authData) {
+	    	if (authData) {
+			    // the access token will allow us to make Open Graph API calls
+			    console.log(authData.facebook.accessToken);
+			  }
+	    }, {
+		  remember: "default",
+		  scope: "email"
+		});
+	  };
+	  scope.logout = function(provider) {
+	    ref.unauth();
+	  };
+    }
+  }
 })
