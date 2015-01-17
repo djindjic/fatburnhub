8d4276b82292d44f6aa358700152726402d8c74a
Aleksandar Djindjic <djindjic@gmail.com>
2015-01-17 13:50:41 +0100

    change project structue

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-git add --all
-git commit -m '[deploy: cloudatcost]'
-git push origin master
--- fbh-back/app/controllers/v1/users_controller.rb
+++
@@ -1,7 +0,0 @@
-module V1
-	class UsersController < ApplicationController
-		def index
-			render json: true
-		end
-	end
-end
