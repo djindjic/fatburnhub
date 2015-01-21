b0c65ea44f42e8a8b07d6dfcc748810a7299ff96
Aleksandar Djindjic <djindjic@gmail.com>
2015-01-21 23:55:40 +0100

    sauce labs with tunel

--- e2e/conf.js
+++ e2e/conf.js
@@ -8,13 +8,25 @@
 
 exports.config = {
   //seleniumAddress: 'http://localhost:4444/wd/hub',
+  // capabilities: {
+  //   'browserName': 'chrome',
+  //   'chromeOptions': {
+  //       args: ['--test-type']
+  //   }
+  // },
   capabilities: {
     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-  	'build': process.env.TRAVIS_BUILD_NUMBER
+  	build: "1",
+    platformName: 'iOS',
+    platformVersion: '7.1',
+    browserName: '',
+    app: 'safari',
+    deviceName: 'iPhone Simulator',
+    'appium-version': '1.2.1'
   },
   specs: ['spec.js'],
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
-  allScriptsTimeout: 99999,
+  sauceUser: process.env.SAUCE_USER_NAME,
+  sauceKey: process.env.SAUCE_API_KEY,
+  //allScriptsTimeout: 99999,
   baseUrl: 'http://localhost:9000' //default test port with Yeoman is 127.0.0.1 (localhost)
 };
