c3ae09d611f8974aded80178d8837f67d95c0b47
Aleksandar Djindjic <djindjic@gmail.com>
2015-01-30 00:01:13 +0100

    capabilities

--- sauce_labs_capabilities.js
+++ sauce_labs_capabilities.js
@@ -1,8 +1,26 @@
 module.exports.capabilities = {
-  sl_chrome: {
+  // sl_chrome: {
+  //   base: 'SauceLabs',
+  //   browserName: 'chrome',
+  //   platform: 'Linux',
+  //   version: '35'
+  // },
+  sl_firefox: {
     base: 'SauceLabs',
-    browserName: 'chrome',
+    browserName: 'firefox',
     platform: 'Linux',
-    version: '38'
+    version: '31'
+  },
+  sl_ios_safari: {
+    base: 'SauceLabs',
+    browserName: 'safari',
+    platform: 'OS X 10.8',
+    version: '6'
+  },
+  sl_ie_9: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    platform: 'Windows 7',
+    version: '9'
   }
 };
