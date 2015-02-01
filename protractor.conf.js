3d7509001b4ed71e74b9cab502b9d47dfee2095f
Aleksandar Djindjic <djindjic@gmail.com>
2015-02-02 00:10:24 +0100

    login test

--- protractor.conf.js
+++ protractor.conf.js
@@ -3,7 +3,6 @@ var capabilities = require('./sauce_labs_capabilities.js').capabilities;
 var configuration = {
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   multiCapabilities: [{
-    //'browserName': 'firefox',
     'browserName': 'chrome'
   }],
 
