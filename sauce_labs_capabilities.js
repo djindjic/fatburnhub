5c30ff22bd280235b2f7d494f9f7b5f1c13dace6
Aleksandar Djindjic <djindjic@gmail.com>
2015-02-02 23:33:42 +0100

    capabilities

--- sauce_labs_capabilities.js
+++ sauce_labs_capabilities.js
@@ -4,8 +4,6 @@ module.exports.capabilities = {
     browserName: 'firefox',
     platform: 'Linux',
     version: '31',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    build: process.env.TRAVIS_BUILD_NUMBER,
     name: "rafburnhub build #{process.env.TRAVIS_BUILD_NUMBER}"
   },
   sl_ios_safari: {
@@ -13,8 +11,6 @@ module.exports.capabilities = {
     browserName: 'safari',
     platform: 'OS X 10.8',
     version: '6',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    build: process.env.TRAVIS_BUILD_NUMBER,
     name: "rafburnhub build #{process.env.TRAVIS_BUILD_NUMBER}"
   },
   sl_ie_9: {
@@ -22,8 +18,6 @@ module.exports.capabilities = {
     browserName: 'internet explorer',
     platform: 'Windows 7',
     version: '9',
-    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    build: process.env.TRAVIS_BUILD_NUMBER,
     name: "rafburnhub build #{process.env.TRAVIS_BUILD_NUMBER}"
   }
 };
