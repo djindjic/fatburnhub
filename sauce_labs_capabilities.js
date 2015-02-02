6c219acd5213670f22803c4f8d1664220dd6a1b4
Aleksandar Djindjic <djindjic@gmail.com>
2015-02-02 23:40:39 +0100

    capabilities

--- sauce_labs_capabilities.js
+++ sauce_labs_capabilities.js
@@ -4,20 +4,20 @@ module.exports.capabilities = {
     browserName: 'firefox',
     platform: 'Linux',
     version: '31',
-    name: "rafburnhub build #{process.env.TRAVIS_BUILD_NUMBER}"
+    name: "faburnhub build " + process.env.TRAVIS_BUILD_NUMBER}
   },
   sl_ios_safari: {
     base: 'SauceLabs',
     browserName: 'safari',
     platform: 'OS X 10.8',
     version: '6',
-    name: "rafburnhub build #{process.env.TRAVIS_BUILD_NUMBER}"
+    name: "faburnhub build " + process.env.TRAVIS_BUILD_NUMBER}
   },
   sl_ie_9: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
     platform: 'Windows 7',
     version: '9',
-    name: "rafburnhub build #{process.env.TRAVIS_BUILD_NUMBER}"
+    name: "faburnhub build " + process.env.TRAVIS_BUILD_NUMBER}
   }
 };
