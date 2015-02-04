0e7fe350907ac93e86de1dd647972ea8b9f77961
Aleksandar Djindjic <djindjic@gmail.com>
2015-02-04 22:50:00 +0100

    try metabolic js

--- sauce_labs_capabilities.js
+++ sauce_labs_capabilities.js
@@ -1,11 +1,11 @@
 module.exports.capabilities = {
-  // sl_firefox: {
-  //   base: 'SauceLabs',
-  //   browserName: 'firefox',
-  //   platform: 'Linux',
-  //   version: '31',
-  //   name: "faburnhub build " + process.env.TRAVIS_BUILD_NUMBER
-  // },
+  sl_firefox: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    platform: 'Linux',
+    version: '31',
+    name: "faburnhub build " + process.env.TRAVIS_BUILD_NUMBER
+  }
   // sl_ios_safari: {
   //   base: 'SauceLabs',
   //   browserName: 'safari',
@@ -13,11 +13,11 @@ module.exports.capabilities = {
   //   version: '6',
   //   name: "faburnhub build " + process.env.TRAVIS_BUILD_NUMBER
   // },
-  sl_ie_9: {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 7',
-    version: '9',
-    name: "faburnhub build " + process.env.TRAVIS_BUILD_NUMBER
-  }
+  // sl_ie_9: {
+  //   base: 'SauceLabs',
+  //   browserName: 'internet explorer',
+  //   platform: 'Windows 7',
+  //   version: '9',
+  //   name: "faburnhub build " + process.env.TRAVIS_BUILD_NUMBER
+  // }
 };
