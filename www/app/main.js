50bb3ecb86ffda6fe02375525db9b75da5325927
Aleksandar Djindjic <djindjic@gmail.com>
2015-02-05 17:17:37 +0100

    import material design before bundle

--- www/app/main.js
+++ www/app/main.js
@@ -1,5 +1,6 @@
 //vendor
 import angular from 'angular';
+import 'angular-material';
 import 'angular-ui-router';
 import fbhFirebaseUtil from 'fbh-firebase-util';
 
@@ -9,7 +10,7 @@ import {mainConstantModule} from './constants/main.constant';
 //configs
 import {mainConfigModule} from './config/main.config';
 import {fbhFirebaseUtilConfigModule} from './config/fbhFirebaseUtil.config'
-//import {angularMaterialConfigModule} from './config/angularMaterial.config'
+import {angularMaterialConfigModule} from './config/angularMaterial.config'
 
 // //routes
 import {dietRouteModule} from './routes/diet/diet.route';
@@ -18,7 +19,7 @@ import {trainingRouteModule} from './routes/training/training.route';
 export let mainModule = angular.module('fatburnhub', [
   'ngMaterial',
   fbhFirebaseUtil.name,
-  //angularMaterialConfigModule.name,
+  angularMaterialConfigModule.name,
   mainConstantModule.name,
   mainConfigModule.name,
   fbhFirebaseUtilConfigModule.name,
