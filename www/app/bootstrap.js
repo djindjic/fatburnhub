7843ccd50a258117c919fdf1e31845e916fda025
Aleksandar Djindjic <djindjic@gmail.com>
2015-02-05 16:36:26 +0100

    import material design before bundle

--- www/app/bootstrap.js
+++ www/app/bootstrap.js
@@ -1,5 +1,4 @@
 import {mainModule} from './main';
-import './styles/material.css!css'
 
 angular.element(document).ready(function() {
   angular.bootstrap(document.querySelector('[data-main-app]'), [
--- www/app/main.js
+++ www/app/main.js
@@ -1,6 +1,5 @@
 //vendor
 import angular from 'angular';
-import 'angular-material';
 import 'angular-ui-router';
 import fbhFirebaseUtil from 'fbh-firebase-util';
 
@@ -10,7 +9,7 @@ import {mainConstantModule} from './constants/main.constant';
 //configs
 import {mainConfigModule} from './config/main.config';
 import {fbhFirebaseUtilConfigModule} from './config/fbhFirebaseUtil.config'
-import {angularMaterialConfigModule} from './config/angularMaterial.config'
+//import {angularMaterialConfigModule} from './config/angularMaterial.config'
 
 // //routes
 import {dietRouteModule} from './routes/diet/diet.route';
@@ -19,7 +18,7 @@ import {trainingRouteModule} from './routes/training/training.route';
 export let mainModule = angular.module('fatburnhub', [
   'ngMaterial',
   fbhFirebaseUtil.name,
-  angularMaterialConfigModule.name,
+  //angularMaterialConfigModule.name,
   mainConstantModule.name,
   mainConfigModule.name,
   fbhFirebaseUtilConfigModule.name,
