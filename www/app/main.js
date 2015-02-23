933cdb0812faac4a91327b756b4bcdf61cf0287e
Aleksandar Djindjic <djindjic@gmail.com>
2015-02-23 11:12:22 +0100

    famouse

--- www/app/main.js
+++ www/app/main.js
@@ -1,6 +1,8 @@
 //vendor
 import angular from 'angular';
 import 'angular-ui-router';
+import 'famous';
+import 'famous-angular';
 
 //styles
 import './styles/material.css!';
@@ -21,6 +23,7 @@ import {dataRouteModule} from './routes/data/data.route';
 import {fbhMainTabsDirectiveModule} from './components/fbhMainTabs/fbhMainTabs.directive';
 
 export let mainModule = angular.module('fatburnhub', [
+  'famous.angular',
   'mockLoginModule',
   mainConstantModule.name,
   angularMaterialConfigModule.name,
@@ -32,6 +35,24 @@ export let mainModule = angular.module('fatburnhub', [
 ]).run(['mocklogin', function(mocklogin) {
 	console.log(mocklogin());
 }]);
+
+mainModule.controller('MainCtrl', ['$scope', '$famous', '$timeout', function($scope, $famous, $timeout){
+  var Transitionable = $famous['famous/transitions/Transitionable'];
+  var t = new Transitionable(0);
+
+  function reset(){
+    t.set(0);
+    t.set(Math.PI * 2.0, {duration: 1750, curve: 'linear'}, function(){
+      $timeout(reset);
+    })
+  }
+
+  reset();
+
+  $scope.getRotateY = function(){
+    return t.get();
+  }
+}])
 
 var mockLogin = angular.module('mockLoginModule', []);
 mockLogin.factory('mocklogin', function() {
